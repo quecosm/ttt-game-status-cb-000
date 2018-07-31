9959fdd34ddbdbbbebaae8a377a502256723340a
Queco SM <sersalazar.marioni@gmail.com>
2018-07-31 19:40:56 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,52 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], #top
+  [3,4,5], #middle
+  [6,7,8], #bottom
+  [0,3,6], #left column
+  [1,4,7], #middle column
+  [2,5,8], #right column
+  [0,4,8], #diagonal descending
+  [2,4,6]  #diagonal ascending 
+  ]
+  
+def full? (board)
+  board.none?{|i| i == " "}
+end
+  
+def draw (board)
+  won?(board)==false && full?(board)==true 
+end
+  
+def won? (board)
+    wi1 = WIN_COMBINATIONS[0]
+    wi2 = WIN_COMBINATIONS[1]
+    wi3 = WIN_COMBINATIONS[2]
+    wi4 = WIN_COMBINATIONS[3]
+    wi5 = WIN_COMBINATIONS[4]
+    wi6 = WIN_COMBINATIONS[5]
+    wi7 = WIN_COMBINATIONS[6]
+    wi8 = WIN_COMBINATIONS[7]
+    
+    top_row = board[wi1]
+    middle_row = board[wi2]
+    bottom_row = board[wi3]
+    left_column = board[wi4]
+    middle_column = board[wi5]
+    right_column = board[wi6]
+    diagonal_descending = board[wi7]
+    diagonal_ascending = board[wi8]
+    
+  board.all? do|i| i != " "
+      end
+      
+end
+
+ 
